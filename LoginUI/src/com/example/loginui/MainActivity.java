46c3a90266c6e80006350785327f3d6d9b7bd7b4
tobiasjr <tobjo423@student.liu.se>
2014-12-19 13:12:24 +0100

    margins of headers

--- LoginUI/src/com/example/loginui/MainActivity.java
+++ LoginUI/src/com/example/loginui/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends ActionBarActivity {
         
         RC = (RegistrationComponent) findViewById(R.id.registration);
         
-        RC.setFieldBackgroundColor(Color.BLACK);
+        RC.setFieldBackgroundColor(Color.RED);
         RC.setTextColor(Color.GREEN);
         RC.setHeaderColor(Color.WHITE);
         RC.addField("New field");
